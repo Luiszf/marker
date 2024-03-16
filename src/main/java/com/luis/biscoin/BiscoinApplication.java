fbc3024b92ba690972569b56059e93232dcbc454
Luis Nascimento <lfernando.s.nascimento@gmail.com>
2024-03-16 00:52:03 -0300

    initial commit spring template

---
+++ src/main/java/com/luis/biscoin/BiscoinApplication.java
@@ -0,0 +1,13 @@
+package com.luis.biscoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiscoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiscoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luis/biscoin/BiscoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.biscoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiscoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
